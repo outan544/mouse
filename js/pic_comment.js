bc66a00791ac54373d60ce3d7889ae5ea2c9dee5
apip <outan544@gmail.com>
2025-03-15 20:25:56 +0700

    Update pic_comment.js

--- js/pic_comment.js
+++ js/pic_comment.js
@@ -1,7 +1,7 @@
 // <![CDATA[
 
     document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".__img").forEach(function (el) {
+    document.querySelectorAll(".comment-content").forEach(function (el) {
         el.innerHTML = el.innerHTML.replace(
             /\*\*\*(https?:\/\/[^\s"'<>]+)/g,
             '<img src="$1" alt="Gambar" style="max-width:200px; height:auto;">'
